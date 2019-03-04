b2ec4bc0b5193cc84167a92eeb9433c7eab570a6
Alex Pavlak <alexander.pavlak@mavs.uta.edu>
2019-03-04 08:38:18 -0600

    FIlled new centers array with information from the assignments

--- Kmeans.py
+++ Kmeans.py
@@ -6,8 +6,14 @@ def mykmeans(X,k,c):
     #add a column for each points cluster to the data 
     pointsWithClusters = np.hstack((X,np.zeros((len(X),1))))
 
+    #create an array to store the sums of points in a cluster
+    #as well as the number of points in the cluster
+    centerSets = np.zeros((k,1+np.size(X,1)))
+
     rowCount=0
-    #for every point in X
+    #These 2 loops perform step #2 from the K-means algorithm
+    #For each point in X, it will calculate and store the closest
+    #center
     for i in X:
         
         #keep track of what center we are currently working with for each
@@ -16,19 +22,30 @@ def mykmeans(X,k,c):
         #calculate the distance from each point to every center in C
         
         for j in c:
-            
+            #calculate the distance to the first center every and assign it
+            #as the closest center
             if(centerCount==0):
                 distanceToClosestCenter = np.linalg.norm(i-j)
+                assignedCenter = 0
                 centerCount += 1
             else:
+                #For every center after the first, calculate the distance
+                #and compare it to the current closest distance to center.
+                #save which ever distance is smaller as the cloesest distance to center
                 distToCenter = np.linalg.norm(i-j)
                 if(distToCenter < distanceToClosestCenter):
                     pointsWithClusters[rowCount][2] = centerCount
                     distanceToClosestCenter = distToCenter
+                    assignedCenter = centerCount
                 centerCount+=1
+        #add the newly assigned points values to the set of points for that cluster
+        centerSets[assignedCenter][0] += 1
+        centerSets[assignedCenter,1:] += i
         rowCount += 1
     np.set_printoptions(suppress=True,linewidth=np.nan,threshold=np.nan)
-    print(pointsWithClusters) 
+    print(pointsWithClusters)
+    print(centerSets)
+    #perform the update step
 
 ###MAIN###
 #Given parameters for generating 1st set of gausian numbers
@@ -42,7 +59,10 @@ dev2 = ([[0.9,0.4],[0.4,0.9]])
 set1 = np.random.multivariate_normal(mean1,dev1,500)
 set2 = np.random.multivariate_normal(mean2,dev2,500)
 
-mykmeans(set1,2,[[10,10],[-10,10]])
+#plt.scatter(set1[:,0],set1[:,1])
+#plt.show()
+
+mykmeans(set1,2,[[10,10],[-10,-10]])
 
 
  
